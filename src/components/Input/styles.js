27ca8686df1334a128bf8f2d656a5cef7989af98
drikadev <adriguti.adriana@hotmail.com>
2022-12-19 12:01:30 -0300

    enviando arquivos do projeto wiki github

---
+++ src/components/Input/styles.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const InputContainer = styled.div`
+    border-radius: 20px;
+
+    height: 62px;
+    width: 80%;
+
+    margin: 20px;
+
+    input{
+        background: transparent;
+        border: 0;
+        outline: 1px solid white;
+        border-radius: 20px;
+        color: #FFFFFF;
+        font-size: 20px;
+        height: 62px;
+        width: 100%;
+        padding-left: 10px;
+    }
+`
